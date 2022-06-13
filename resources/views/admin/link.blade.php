365c920f7d5011a8284d50e35d309928d055a86a
bangef <ficrihnp@gmail.com>
2022-06-13 09:53:58 +0700

    add layouting admin

--- resources/views/admin/link.blade.php
+++ resources/views/admin/link.blade.php
@@ -1,9 +1,9 @@
     <!-- Favicon icon -->
-    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('images/favicon.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ url('images/favicon.png') }}">
     <!-- Pignose Calender -->
-    <link href="{{ asset('plugins/pg-calendar/css/pignose.calendar.min.css') }}" rel="stylesheet">
+    <link href="{{ url('plugins/pg-calendar/css/pignose.calendar.min.css') }}" rel="stylesheet">
     <!-- Chartist -->
-    <link rel="stylesheet" href="{{ asset('plugins/chartist/css/chartist.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('plugins/chartist-plugin-tooltips/css/chartist-plugin-tooltip.css') }}">
+    <link rel="stylesheet" href="{{ url('plugins/chartist/css/chartist.min.css') }}">
+    <link rel="stylesheet" href="{{ url('plugins/chartist-plugin-tooltips/css/chartist-plugin-tooltip.css') }}">
     <!-- Custom Stylesheet -->
-    <link href="{{ asset('css/style_admin.css') }}" rel="stylesheet">
+    <link href="{{ url('css/style_admin.css') }}" rel="stylesheet">
