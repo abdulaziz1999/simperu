30cdf1112118324b9ead6a79e5114fea25c10240
abdulaziz <abdu19010ti@student.nurulfikri.ac.id>
2022-06-27 22:27:35 +0700

    controllers dashboard admin

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Gedung;
+use App\Models\Ruangan;
+use App\Models\KategoriRuangan;
+use App\Models\Fasilitas;
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    public function index()
+    {
+        //get all data gedung order by id desc
+        $gedung = Gedung::all();
+        $ruangan = Ruangan::all();
+        $fasilitas = Fasilitas::all();
+        $kategoriRuangan = KategoriRuangan::all();
+        return view('admin-dashboard.index', compact('gedung','ruangan','kategoriRuangan','fasilitas'));
+    }
+}
