2a6718d7c0e119a05a2d1554090f64ece2476592
abdulaziz <abdu19010ti@student.nurulfikri.ac.id>
2022-07-18 07:49:01 +0700

    add js chart js peminjaman dash

--- resources/views/admin/script.blade.php
+++ resources/views/admin/script.blade.php
@@ -7,27 +7,30 @@
     <script src="{{ asset('js/gleek.js') }}"></script>
     <script src="{{ asset('js/styleSwitcher.js') }}"></script>
 
+    @if(Request::segment(1) == 'admin')
     <!-- Chartjs -->
     <script src="{{ asset('plugins/chart.js/Chart.bundle.min.js') }}"></script>
+    <script src="{{ asset('js/plugins-init/chartjs-init.js') }}"></script>
+    @endif
     <!-- Circle progress -->
     <script src="{{ asset('plugins/circle-progress/circle-progress.min.js') }}"></script>
     <!-- Datamap -->
-    <script src="{{ asset('plugins/d3v3/index.js') }}"></script>
+    <!-- <script src="{{ asset('plugins/d3v3/index.js') }}"></script>
     <script src="{{ asset('plugins/topojson/topojson.min.js') }}"></script>
-    <script src="{{ asset('plugins/datamaps/datamaps.world.min.js') }}"></script>
+    <script src="{{ asset('plugins/datamaps/datamaps.world.min.js') }}"></script> -->
     <!-- Morrisjs -->
-    <script src="{{ asset('plugins/raphael/raphael.min.js') }}"></script>
-    <script src="{{ asset('plugins/morris/morris.min.js') }}"></script>
+    <!-- <script src="{{ asset('plugins/raphael/raphael.min.js') }}"></script>
+    <script src="{{ asset('plugins/morris/morris.min.js') }}"></script> -->
     <!-- Pignose Calender -->
-    <script src="{{ asset('plugins/moment/moment.min.js') }}"></script>
-    <script src="{{ asset('plugins/pg-calendar/js/pignose.calendar.min.js') }}"></script>
+    <!-- <script src="{{ asset('plugins/moment/moment.min.js') }}"></script>
+    <script src="{{ asset('plugins/pg-calendar/js/pignose.calendar.min.js') }}"></script> -->
     <!-- ChartistJS -->
-    <script src="{{ asset('plugins/chartist/js/chartist.min.js') }}"></script>
-    <script src="{{ asset('plugins/chartist-plugin-tooltips/js/chartist-plugin-tooltip.min.js') }}"></script>
+    <!-- <script src="{{ asset('plugins/chartist/js/chartist.min.js') }}"></script>
+    <script src="{{ asset('plugins/chartist-plugin-tooltips/js/chartist-plugin-tooltip.min.js') }}"></script> -->
 
 
 
-    <script src="{{ asset('js/dashboard/dashboard-1.js') }}"></script>
+    <!-- <script src="{{ asset('js/dashboard/dashboard-1.js') }}"></script> -->
 
     {{-- plugins datatable --}}
     <script src="{{ asset('plugins/tables/js/jquery.dataTables.min.js') }}"></script>
