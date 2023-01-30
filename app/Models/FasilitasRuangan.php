4a363d312ac92a9d1e8b4b5fb81253025c532ef6
abdulaziz <abdu19010ti@student.nurulfikri.ac.id>
2023-01-30 20:39:10 +0700

    model fasilitas ruangan

---
+++ app/Models/FasilitasRuangan.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class FasilitasRuangan extends Model
+{
+    use HasFactory;
+    protected $table = 'fasilitas_ruangan';
+
+    protected $guarded = ['id'];
+
+    // Relasi many to one
+    // banyak fasilitas dimemiliki satu ruangan
+    public function ruangan()
+    {
+        return $this->belongsTo(Ruangan::class);
+    }
+}
