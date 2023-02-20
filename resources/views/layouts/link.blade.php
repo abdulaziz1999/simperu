e6623c602e877192a33341def6d282f8a0e8f348
abdulaziz <abdu19010ti@student.nurulfikri.ac.id>
2023-02-20 22:51:53 +0700

    url assets

--- resources/views/layouts/link.blade.php
+++ resources/views/layouts/link.blade.php
@@ -1,5 +1,5 @@
 <!-- Favicon -->
-    <link rel="icon" type="image/png" sizes="16x16" href="{{ url('/public/img/favicon-16x16.png')}}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('img/favicon-16x16.png')}}">
     <!-- Google Web Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -7,14 +7,14 @@
     <!-- Icon Font Stylesheet -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <!-- Libraries Stylesheet -->
-    <link href="{{ url('public/lib/animate/animate.min.css') }}" rel="stylesheet">
-    <link href="{{ url('public/lib/owlcarousel/assets/owl.carousel.min.css') }}" rel="stylesheet">
-    <link href="{{ url('public/lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('lib/animate/animate.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('lib/owlcarousel/assets/owl.carousel.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css') }}" rel="stylesheet" />
     <!-- Customized Bootstrap Stylesheet -->
-    <link href="{{ url('public/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
     <!-- Template Stylesheet -->
-    <link href="{{ url('public/css/style.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
     {{-- hover-third-part --}}
-    <link href="{{ url('public/css/hover.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/hover.css') }}" rel="stylesheet">
     {{-- jquery --}}
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
